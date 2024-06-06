3a12823ac74fb65c99a986c3ff2a86db3b4e97f7
nandkishorr <rachulr7@gmail.com>
2024-06-06 16:31:58 +0530

    get movies initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./Components/Home"
+import Navbar from "./Components/Navbar"
+
+function App() {
+
+
+  return (
+  <div className="h-screen w-full">
+            <Navbar/>
+            <Home/>
+  </div>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div className="h-full bg-[#F5F5F5]">
+        
+     
+    </div>
+  )
+}
+
+export default Home
