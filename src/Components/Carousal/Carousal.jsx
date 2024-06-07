2a9c18d8fe54811fc8d5ebcca816f71e85b73691
nandkishorr <rachulr7@gmail.com>
2024-06-07 07:33:19 +0530

    carousal page updated

--- src/Components/Carousal/Carousal.jsx
+++ src/Components/Carousal/Carousal.jsx
@@ -19,7 +19,7 @@ const Carousel = () => {
     <div className="carousel-container">
       <Slider {...settings}>
         <div className="carousel-slide ">
-          <img src={Image} alt="Slide 1" />
+          <img  src={Image} alt="Slide 1" />
           <div className="carousel-caption">
             <h1>Spider-Man: Into The Spider Verse</h1>
             <p>
@@ -32,11 +32,11 @@ const Carousel = () => {
               </div>
         </div>
         <div className="carousel-slide ">
-          <img src={Image} alt="Slide 1" />
+          <img src='https://images-na.ssl-images-amazon.com/images/M/MV5BNTM1NjYyNTY5OV5BMl5BanBnXkFtZTcwMjgwNTMzMQ@@._V1_SX300.jpg' alt="Slide 2" />
           <div className="carousel-caption">
-            <h1>Spider-Man: Into The Spider Verse</h1>
+            <h1>Apocalypto</h1>
             <p>
-              Spider-Man: Across the Spider-Verse, now zipping into the theater-verse, is the long-awaited follow-up to 2018's "Spider-Man: Into the Spider-Verse," a revelatory thrill ride that deservedly won the Oscar for animation.
+            The Mayan kingdom is at the height of its opulence and power but the foundations of the empire are beginning to crumble. The leaders believe they must build more temples and sacrifice more people or their crops and citizens will die.
                </p>
              <button className="watch-trailer-button flex gap-1 items-center"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM9.5547 7.16795C9.24784 6.96338 8.8533 6.94431 8.52814 7.11833C8.20298 7.29235 8 7.63121 8 8V12C8 12.3688 8.20298 12.7077 8.52814 12.8817C8.8533 13.0557 9.24784 13.0366 9.5547 12.8321L12.5547 10.8321C12.8329 10.6466 13 10.3344 13 10C13 9.66565 12.8329 9.35342 12.5547 9.16795L9.5547 7.16795Z" fill="#FF0059"/>
@@ -45,11 +45,11 @@ const Carousel = () => {
               </div>
         </div>
         <div className="carousel-slide ">
-          <img src={Image} alt="Slide 1" />
+          <img src='https://images-na.ssl-images-amazon.com/images/M/MV5BN2ViYTFiZmUtOTIxZi00YzIxLWEyMzUtYjQwZGNjMjNhY2IwXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg' alt="Slide 1" />
           <div className="carousel-caption">
-            <h1>Spider-Man: Into The Spider Verse</h1>
+            <h1>The Beach</h1>
             <p>
-              Spider-Man: Across the Spider-Verse, now zipping into the theater-verse, is the long-awaited follow-up to 2018's "Spider-Man: Into the Spider-Verse," a revelatory thrill ride that deservedly won the Oscar for animation.
+            Garland's novel centers on a young nicotine-addicted traveler named Richard, an avid pop-culture buff with a particular love for video games and Vietnam War movies
                </p>
              <button className="watch-trailer-button flex gap-1 items-center"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM9.5547 7.16795C9.24784 6.96338 8.8533 6.94431 8.52814 7.11833C8.20298 7.29235 8 7.63121 8 8V12C8 12.3688 8.20298 12.7077 8.52814 12.8817C8.8533 13.0557 9.24784 13.0366 9.5547 12.8321L12.5547 10.8321C12.8329 10.6466 13 10.3344 13 10C13 9.66565 12.8329 9.35342 12.5547 9.16795L9.5547 7.16795Z" fill="#FF0059"/>
@@ -58,11 +58,11 @@ const Carousel = () => {
               </div>
         </div>
         <div className="carousel-slide ">
-          <img src={Image} alt="Slide 1" />
+          <img src='https://images-na.ssl-images-amazon.com/images/M/MV5BMTU1MDA1MTYwMF5BMl5BanBnXkFtZTcwMDcxMzA1Mg@@._V1_SX300.jpg' alt="Slide 1" />
           <div className="carousel-caption">
-            <h1>Spider-Man: Into The Spider Verse</h1>
+            <h1>The Hangover</h1>
             <p>
-              Spider-Man: Across the Spider-Verse, now zipping into the theater-verse, is the long-awaited follow-up to 2018's "Spider-Man: Into the Spider-Verse," a revelatory thrill ride that deservedly won the Oscar for animation.
+            Two days before his wedding, Doug (Justin Bartha) and three friends (Bradley Cooper, Ed Helms, Zach Galifianakis) drive to Las Vegas for a wild and
                </p>
              <button className="watch-trailer-button flex gap-1 items-center"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM9.5547 7.16795C9.24784 6.96338 8.8533 6.94431 8.52814 7.11833C8.20298 7.29235 8 7.63121 8 8V12C8 12.3688 8.20298 12.7077 8.52814 12.8817C8.8533 13.0557 9.24784 13.0366 9.5547 12.8321L12.5547 10.8321C12.8329 10.6466 13 10.3344 13 10C13 9.66565 12.8329 9.35342 12.5547 9.16795L9.5547 7.16795Z" fill="#FF0059"/>
@@ -71,11 +71,11 @@ const Carousel = () => {
               </div>
         </div>
         <div className="carousel-slide ">
-          <img src={Image} alt="Slide 1" />
+          <img src='https://images-na.ssl-images-amazon.com/images/M/MV5BMzU0NDY0NDEzNV5BMl5BanBnXkFtZTgwOTIxNDU1MDE@._V1_SX300.jpg' alt="Slide 1" />
           <div className="carousel-caption">
-            <h1>Spider-Man: Into The Spider Verse</h1>
+            <h1>The Hobbit: The Desolation of Smaug</h1>
             <p>
-              Spider-Man: Across the Spider-Verse, now zipping into the theater-verse, is the long-awaited follow-up to 2018's "Spider-Man: Into the Spider-Verse," a revelatory thrill ride that deservedly won the Oscar for animation.
+            The dwarves, along with Bilbo Baggins and Gandalf the Grey, continue their quest to reclaim the Lonely Mountain, their homeland, from the dragon Smaug.
                </p>
              <button className="watch-trailer-button flex gap-1 items-center"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM9.5547 7.16795C9.24784 6.96338 8.8533 6.94431 8.52814 7.11833C8.20298 7.29235 8 7.63121 8 8V12C8 12.3688 8.20298 12.7077 8.52814 12.8817C8.8533 13.0557 9.24784 13.0366 9.5547 12.8321L12.5547 10.8321C12.8329 10.6466 13 10.3344 13 10C13 9.66565 12.8329 9.35342 12.5547 9.16795L9.5547 7.16795Z" fill="#FF0059"/>
@@ -84,11 +84,11 @@ const Carousel = () => {
               </div>
         </div>
         <div className="carousel-slide ">
-          <img src={Image} alt="Slide 1" />
+          <img src='https://images-na.ssl-images-amazon.com/images/M/MV5BMjIxMjgxNTk0MF5BMl5BanBnXkFtZTgwNjIyOTg2MDE@._V1_SX300.jpg' alt="Slide 1" />
           <div className="carousel-caption">
-            <h1>Spider-Man: Into The Spider Verse</h1>
+            <h1>The Wolf of Wall Street</h1>
             <p>
-              Spider-Man: Across the Spider-Verse, now zipping into the theater-verse, is the long-awaited follow-up to 2018's "Spider-Man: Into the Spider-Verse," a revelatory thrill ride that deservedly won the Oscar for animation.
+            Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.
                </p>
              <button className="watch-trailer-button flex gap-1 items-center"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM9.5547 7.16795C9.24784 6.96338 8.8533 6.94431 8.52814 7.11833C8.20298 7.29235 8 7.63121 8 8V12C8 12.3688 8.20298 12.7077 8.52814 12.8817C8.8533 13.0557 9.24784 13.0366 9.5547 12.8321L12.5547 10.8321C12.8329 10.6466 13 10.3344 13 10C13 9.66565 12.8329 9.35342 12.5547 9.16795L9.5547 7.16795Z" fill="#FF0059"/>
