bd6ab12a50ed6e445e574a78d4dcbe7ab9b4f71b
NANDKISHOR R <96693626+nandkishorr@users.noreply.github.com>
2024-06-07 08:17:47 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
- export const BASE_URL = 'http://localhost:3000';
-//export const BASE_URL = 'https://getmovies-backend-a11o.onrender.com';
+ //export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://getmovies-backend-a11o.onrender.com';
